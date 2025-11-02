d2aaea49fd12f4c10eb04d017330e4584ad3e963
Your Name <your.email@example.com>
2025-11-02 15:08:49 +0530

    commit - 202511020003

--- myproject/settings.py
+++ myproject/settings.py
@@ -121,7 +121,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.25.2:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'core.utils.custom_exception_handler'
